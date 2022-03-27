9d1f18ddd09b29c1d8f8e43584e310b8123089dd
Matthew Deaves <mndeaves@me.com>
2022-03-27 11:12:12 +0100

    more lesson 6 examples

--- SORGENTI2/LEZIONE6c.s
+++ SORGENTI2/LEZIONE6c.s
@@ -1,3 +1,4 @@
+;APS00000000000000000000000000000000000000000000000000000000000000000000000000000000
 
 ; Lezione6c.s	STAMPIAMO VARIE RIGHE DI TESTO SULLO SCHERMO!!!
 
@@ -25,7 +26,7 @@ Inizio:
 	move.w	#0,$dff1fc		; Disattiva l'AGA
 	move.w	#$c00,$dff106		; Disattiva l'AGA
 
-	bsr.w	print		; Stampa le linee di testo sullo schermo
+	bsr.w	PRINT		; Stampa le linee di testo sullo schermo
 
 mouse:
 	btst	#6,$bfe001	; tasto sinistro del mouse premuto?
@@ -36,7 +37,7 @@ mouse:
 
 	move.l	4.w,a6
 	jsr	-$7e(a6)	; Enable - riabilita il Multitasking
-	move.l	gfxbase(PC),a1	; Base della libreria da chiudere
+	move.l	GfxBase(PC),a1	; Base della libreria da chiudere
 	jsr	-$19e(a6)	; Closelibrary - chiudo la graphics lib
 	rts			; USCITA DAL PROGRAMMA
 
@@ -118,7 +119,7 @@ TESTO:	     ;		  1111111111222222222233333333334
 	dc.b	'                                        ' ; 22
 	dc.b	'  AHI Quanto a DIR QUAL ERA...          ' ; 23
 	dc.b	'                                        ' ; 24
-	dc.b	'                                        ' ; 25
+	dc.b	'  This is a test by Matt                ' ; 25
 	dc.b	'                                        ' ; 26
 	dc.b	'                                        ' ; 27
 
@@ -194,7 +195,7 @@ BPLPOINTERS:
 ;	Il FONT caratteri 8x8
 
 FONT:
-	incbin	"metal.fnt"	; Carattere largo
+	incbin	"hd1:develop/projects/dischi/SORGENTI2/metal.fnt"	; Carattere largo
 ;	incbin	"normal.fnt"	; Simile ai caratteri kickstart 1.3
 ;	incbin	"nice.fnt"	; Carattere stretto
 
